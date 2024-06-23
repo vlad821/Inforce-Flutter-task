69f513330237b44ee47c5de993ed164f1f427b3f
vlad821 <115928876+vlad821@users.noreply.github.com>
2024-06-23 14:27:18 +0300

    change colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color primaryColor = Color.fromARGB(255, 202, 226, 247);
   static const Color textColor = Color.fromARGB(255, 0, 13, 23);
-  static const Color whiteColor = Color.fromARGB(255, 255, 255, 255); 
-  static const Color addItemButtonColor = Color.fromARGB(255, 5, 102, 11); 
-  static const Color deleteButtonColor = Colors.red; 
+  static const Color whiteColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color addItemButtonColor = Color.fromARGB(255, 63, 173, 39);
+  static const Color deleteButtonColor = Color.fromARGB(255, 240, 47, 33);
 }
