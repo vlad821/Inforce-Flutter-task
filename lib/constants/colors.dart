125e33a7a3589bccf521169ebb1a6aab00cd252e
vlad821 <115928876+vlad821@users.noreply.github.com>
2024-06-23 12:35:54 +0300

    moved the color to a separate file

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color.fromARGB(255, 202, 226, 247);
+  static const Color textColor = Color.fromARGB(255, 0, 13, 23);
+  static const Color whiteColor = Color.fromARGB(255, 255, 255, 255); 
+  static const Color addItemButtonColor = Color.fromARGB(255, 5, 102, 11); 
+  static const Color deleteButtonColor = Colors.red; 
+}
